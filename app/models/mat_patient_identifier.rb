8e86efa6687e196a50ac978883b38e25e145b960
blessings <blessings.mhango@gmail.com>
2013-01-31 14:37:35 +0200

    working on error handling

--- app/models/mat_patient_identifier.rb
+++ app/models/mat_patient_identifier.rb
@@ -1,7 +1,7 @@
 class MatPatientIdentifier < ActiveRecord::Base
   establish_connection "openmrs_mat_#{Rails.env}"
   self.table_name = 'patient_identifier'
-  self.primary_key = 'patient_identifier_id'
+  self.primary_key = 'patient_id'
   belongs_to :patient, :class_name => "MatPatient",
              :foreign_key => :patient_id, :conditions => {:voided => 0}
 end
--- app/models/person_name_code.rb
+++ app/models/person_name_code.rb
@@ -1,17 +1,16 @@
+LogErr = Logger.new(Rails.root.join("log","error.txt"))
 class PersonNameCode < ActiveRecord::Base
-
   belongs_to :person, :foreign_key => :id
-
   def self.create_name_code(person)
     found = self.find_by_person_id(person.id)
     if found.blank?
-      self.create(:person_id => person.id,
-                :given_name_code => person.given_name.soundex,
-                :family_name_code => person.family_name.soundex)
+      self.create!(:person_id => person.id,
+                :given_name_code => (person.given_name || '').soundex,
+                :family_name_code => (person.family_name ||'').soundex) rescue LogErr.error person.id
     else
-      found.given_name_code = person.given_name.soundex
-      found.family_name_code = person.family_name.soundex
-      found.save
+      found.given_name_code = (person.given_name || '').soundex
+      found.family_name_code = (person.family_name ||'').soundex
+      found.save! rescue LogErr.error person.id
     end           
   end
 
